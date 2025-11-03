e31495414b027b8198639e15f4e26a5a0cc8d405
MarcBaume <96970620+MarcBaume@users.noreply.github.com>
2025-11-03 08:07:43 +0100

    ménage

--- onglets.php
+++
@@ -1,8 +0,0 @@
-<?php $today = date("Y-m-d H:i:s"); 
-date_default_timezone_set('Europe/Paris');
-// --- La setlocale() fonctionnne pour strftime mais pas pour DateTime->format()
-setlocale(LC_TIME, 'fr_FR.utf8','fra');// OK ?>
-     <div id="logo">
-
-	<img src="images/logoBanderole.png" alt=""  align="right"  />
-   </div>

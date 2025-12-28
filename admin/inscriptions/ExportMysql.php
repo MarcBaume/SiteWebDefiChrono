268a62112b9834030af26bb4c3e9aefbe288f9b5
MarcBaume <96970620+MarcBaume@users.noreply.github.com>
2025-12-28 06:57:47 +0100

    export mysql format windows

--- admin/inscriptions/ExportMysql.php
+++ admin/inscriptions/ExportMysql.php
@@ -1,14 +1,4 @@
  <?php 
- $today = date("Y-m-d H:i:s"); 
-date_default_timezone_set('Europe/Paris');
-// --- La setlocale() fonctionnne pour strftime mais pas pour DateTime->format()
-setlocale(LC_TIME, 'fr_FR.utf8','fra');// OK
- session_start(); 
-
-
-
-
- 
 if ( strlen($_POST['DateCourse'])>0)
 {
 $DateCourse =  $_POST['DateCourse'];
@@ -35,14 +25,12 @@ $NOM_COURSE = $_GET["NomCourse"];
 $Nbr_etape =  $_GET["NbrEtape"] ;
 
 }
-  
+ 
 $filename = "Export".$NOM_COURSE. $ANNEE_COURSE.".csv";
 if (file_exists($filename)) 
 {
 	unlink($filename);
 }
-
- 
   // On se connecte à MySQL
  $con = mysqli_connect('dxvv.myd.infomaniak.com', 'dxvv_christopheJ', 'er3z4aet1234');
 if (!$con)
@@ -53,50 +41,57 @@ if (!$con)
   {
 	mysqli_select_db($con ,'dxvv_jurachrono' );
 	if ($_POST['parcours']==("")){
-     $sql = 'SELECT * FROM inscription WHERE course=\''.$NOM_COURSE. $ANNEE_COURSE .'\'ORDER BY nom ASC';
+     	$sql = 'SELECT * FROM inscription WHERE course=\''.$NOM_COURSE. $ANNEE_COURSE .'\'ORDER BY nom ASC';
 	}
 	else{
-	 $sql = 'SELECT * FROM inscription  WHERE course=\''.$NOM_COURSE. $ANNEE_COURSE. '\'AND NomDepart = \''.$_POST["parcours"]. '\'ORDER BY nom ASC';
+	 	$sql = 'SELECT * FROM inscription  WHERE course=\''.$NOM_COURSE. $ANNEE_COURSE. '\'AND NomDepart = \''.$_POST["parcours"]. '\'ORDER BY nom ASC';
 	 }
 	 
-	 $result = mysqli_query($con,$sql);
-	 $HeaderCol = 0;
-	  $new_csv = fopen($filename, 'w');
-	  if ($result && mysqli_num_rows($result) > 0) 
-	  {
-		
-		
-		
+	$result = mysqli_query($con,$sql);
+	$HeaderCol = 0;
+	header('Content-Type: text/csv; charset=windows-1252');
+	header("Content-disposition: attachment;filename=\"" . "Export".$NOM_COURSE. $ANNEE_COURSE.".csv". "\""); 
+	// Source - https://stackoverflow.com/a/24545434
+// Posted by ecnepsnai, modified by community. See post 'Timeline' for change history
+// Retrieved 2025-12-25, License - CC BY-SA 4.0
+
+	$new_csv = fopen('php://output', 'w');
+	if ($result && mysqli_num_rows($result) > 0) 
+	{
+		$header = 0;
 		// output data of each row
 		while($row = mysqli_fetch_assoc($result)) 
-		{
-			 
-			$LineHeader = "";
-			 foreach($row as $key => $value)
+		{ 
+			$line = "";
+			$ligne_ansi = "";
+			
+			// En tête des colonnes
+			if ($header == 0)
 			{
-				if ($HeaderCol == 0)
+				$header = 1;
+				foreach($row as $key => $value)
 				{
-					$LineHeader = $LineHeader. $key.";";
-					
+					$line = $line. $key.";29";				
 				}
+				$lineAnsi = iconv("UTF-8", "Windows-1252", $line);
+				fwrite($new_csv, $lineAnsi."\n");
 			}
-			if ($HeaderCol == 0)
+			else
 			{
-				fwrite($new_csv, $LineHeader . "\n" );
-		
-				$HeaderCol = 1;
+				// Valeurs des colonnes
+				foreach($row as $current => $value)
+				{
+					$line = $line. $value.";";				
+				}
+				$lineAnsi = iconv("UTF-8", "Windows-1252", $line);
+				fwrite($new_csv, $lineAnsi."\n");
 			}
-			fputcsv($new_csv, $row, ';');
 		}
-		 
-	  }
-  fclose($new_csv);	
-  $uploads_dir = '/uploads';
-  
-  header('Content-Type: application/octet-stream');
-header("Content-Transfer-Encoding: Binary"); 
-header("Content-disposition: attachment; filename=\"" . "Export".$NOM_COURSE. $ANNEE_COURSE.".csv". "\""); 
-readfile($filename); 
+	}
+
+  	fclose($new_csv);	
+  	//$uploads_dir = '/uploads';
+	//readfile($filename); 
 
 
      // force download  
